3db8e9682098c1cd3a93f292ec496f867f6679df
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2022-10-16 23:16:31 +0530

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Home() {
+
+  return (
+    <>
+      <Link href="style1"><a>Style 1</a></Link>
+      <br />
+      <Link href="style2"><a>Style 2</a></Link>
+    </>
+  )
+}
---
+++ pages/style1.js
@@ -0,0 +1,70 @@
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import ReactCanvasConfetti from "react-canvas-confetti";
+
+const canvasStyles = {
+  position: "fixed",
+  pointerEvents: "none",
+  width: "100%",
+  height: "100%",
+  top: 0,
+  left: 0
+};
+
+function getAnimationSettings(angle, originX) {
+  return {
+    particleCount: 3,
+    angle,
+    spread: 55,
+    origin: { x: originX },
+    colors: ["#5AB510", "#10631D"]
+  };
+}
+
+export default function Style1() {
+  const refAnimationInstance = useRef(null);
+  const [intervalId, setIntervalId] = useState();
+
+  const getInstance = useCallback((instance) => {
+    refAnimationInstance.current = instance;
+  }, []);
+
+  const nextTickAnimation = useCallback(() => {
+    if (refAnimationInstance.current) {
+      refAnimationInstance.current(getAnimationSettings(60, 0));
+      refAnimationInstance.current(getAnimationSettings(120, 1));
+    }
+  }, []);
+
+  const startAnimation = useCallback(() => {
+    if (!intervalId) {
+      setIntervalId(setInterval(nextTickAnimation, 16));
+    }
+  }, [nextTickAnimation, intervalId]);
+
+  const pauseAnimation = useCallback(() => {
+    clearInterval(intervalId);
+    setIntervalId(null);
+  }, [intervalId]);
+
+  const stopAnimation = useCallback(() => {
+    clearInterval(intervalId);
+    setIntervalId(null);
+    refAnimationInstance.current && refAnimationInstance.current.reset();
+  }, [intervalId]);
+
+  useEffect(() => {
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [intervalId]);
+  return (
+    <>
+      <div>
+        <button onClick={startAnimation}>Start</button>
+        <button onClick={pauseAnimation}>Pause</button>
+        <button onClick={stopAnimation}>Stop</button>
+      </div>
+      <ReactCanvasConfetti refConfetti={getInstance} style={canvasStyles} />
+    </>
+  )
+}
